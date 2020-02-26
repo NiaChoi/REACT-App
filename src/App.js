3beecef84bf18b28c58164877903682f3b48fb17
unknown <y.nia.choi@gmail.com>
2020-02-26 23:37:20 +0900

    components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,8 @@
 import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import './App.css';
-
-class Subject extends Component {
-  render(){
-    return (
-      //return 안에는 상위태그 하나만
-      <header>
-            <h1>{this.props.title}</h1>
-            {this.props.sub}
-        </header>
-    );
-  }
-}
-
-class TOC extends Component{
-  render(){
-    return(
-      <nav>
-            <ul>
-                <li><a href="1.html">HTML</a></li>
-                <li><a href="2.html">CSS</a></li>
-                <li><a href="3.html">JavaScript</a></li>
-            </ul>
-    
-        </nav>
-
-    );
-  }
-}
-
-class Content extends Component{
-  render(){
-    return(
-        <article>
-            <h2>HTML</h2>
-            HTML is HyperText Markup Language.
-        </article>
-    );
-  }
-}
 
 class App extends Component {
   render() {
@@ -46,7 +10,7 @@ class App extends Component {
       <div className="App">
         <Subject title = "WEB" sub = "World wide web!"></Subject>
         <TOC></TOC>
-        <Content></Content>
+        <Content title = "HTML" desc = "HTML is HyperText Markup Lanquage." ></Content>
       </div>
     );
   }
